6572270ec4227f6867219bf459571df4450e701b
liav2002 <liavmail00@gmail.com>
2024-02-18 21:50:22 +0200

    finished tagged translated docs.

--- Task3/main.py
+++ Task3/main.py
@@ -1,36 +1,44 @@
 import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from tqdm import tqdm
+import json
 
 
-def analyze_sentiment(text):
+def analyze_sentiment_with_progress(text, pbar, result_dict):
     sid = SentimentIntensityAnalyzer()
     scores = sid.polarity_scores(text)
     compound_score = scores['compound']
 
     if compound_score >= 0.05:
-        return 'Positive'
+        sentiment = 'Positive'
     elif compound_score <= -0.05:
-        return 'Negative'
+        sentiment = 'Negative'
     else:
-        return 'Neutral'
+        sentiment = 'Neutral'
+
+    # Update the progress bar by 1
+    pbar.update(1)
+
+    # Update result dictionary
+    result_dict[sentiment] += 1
+
+    return sentiment
 
 
-def process_excel_file(file_path):
+def process_excel_file(file_path, result_dict):
     df = pd.read_excel(file_path)
-    df['Sentiment'] = df['content'].apply(analyze_sentiment)
 
-    positive_count = df[df['Sentiment'] == 'Positive'].shape[0]
-    negative_count = df[df['Sentiment'] == 'Negative'].shape[0]
+    # Initialize tqdm with the total number of rows
+    with tqdm(total=len(df), desc=f"Processing {file_path}", bar_format="{l_bar}{bar:10}{r_bar}", colour='blue') as pbar:
+        # Apply sentiment analysis for each row
+        df['Sentiment'] = df['content'].apply(lambda x: analyze_sentiment_with_progress(x, pbar, result_dict))
 
+    # Saving to a new file with tagged sentiments
     group_id = file_path.split('_')[-1][0]
-
-    df.to_excel(f"tagged_translated_{group_id}.xlsx", index=False)
-
-    return group_id, [positive_count, negative_count]
+    df.to_excel(f"./output/tagged_translated_15000_docs/tagged_translated_{group_id}.xlsx", index=False)
 
 
 def main():
-    result = {'A': [0, 0], 'B': [0, 0], 'C': [0, 0]}
     file_paths = [
         "./data/translated_15000_docs/translated_A.xlsx",
         "./data/translated_15000_docs/translated_B.xlsx",
@@ -38,11 +46,13 @@ def main():
     ]
 
     for file_path in file_paths:
-        group_id, sentiment_counts = process_excel_file(file_path)
-        result[group_id][0] += sentiment_counts[0]
-        result[group_id][1] += sentiment_counts[1]
+        group_id = file_path.split('_')[-1][0]
+        result_dict = {'Positive': 0, 'Negative': 0, 'Neutral': 0}
+        process_excel_file(file_path, result_dict)
+        with open(f"./output/tagged_translated_15000_docs/result_{group_id}.json", 'w') as json_file:
+            json.dump(result_dict, json_file)
 
-    print("Result:", result)
+    print("Sentiment analysis completed for all files.")
 
 
 if __name__ == "__main__":
