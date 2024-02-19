935326edfe4690e7aa83e041680ab4b40a524d25
liav2002 <liavmail00@gmail.com>
2024-02-19 15:44:50 +0200

    fix data

--- Task3/main.py
+++ Task3/main.py
@@ -22,7 +22,7 @@ def analyze_sentiment_with_progress(text, pbar, result_dict):
     # Update result dictionary
     result_dict[sentiment] += 1
 
-    return sentiment
+    return sentiment, compound_score, scores['neg'], scores['pos'], scores['neu']
 
 
 def process_excel_file(file_path, result_dict):
@@ -31,11 +31,15 @@ def process_excel_file(file_path, result_dict):
     # Initialize tqdm with the total number of rows
     with tqdm(total=len(df), desc=f"Processing {file_path}", bar_format="{l_bar}{bar:10}{r_bar}", colour='blue') as pbar:
         # Apply sentiment analysis for each row
-        df['Sentiment'] = df['content'].apply(lambda x: analyze_sentiment_with_progress(x, pbar, result_dict))
+        df[['Sentiment', 'Compound_Score', 'Neg_Score', 'Pos_Score', 'Neu_Score']] = df['content'].apply(
+            lambda x: pd.Series(analyze_sentiment_with_progress(x, pbar, result_dict)))
 
     # Saving to a new file with tagged sentiments
     group_id = file_path.split('_')[-1][0]
-    df.to_excel(f"./output/tagged_translated_15000_docs/tagged_translated_{group_id}.xlsx", index=False)
+    output_file_path = f"./output/tagged_translated_15000_docs/tagged_translated_{group_id}.xlsx"
+    df.to_excel(output_file_path, index=False)
+
+    return output_file_path
 
 
 def main():
@@ -48,7 +52,7 @@ def main():
     for file_path in file_paths:
         group_id = file_path.split('_')[-1][0]
         result_dict = {'Positive': 0, 'Negative': 0, 'Neutral': 0}
-        process_excel_file(file_path, result_dict)
+        output_file_path = process_excel_file(file_path, result_dict)
         with open(f"./output/tagged_translated_15000_docs/result_{group_id}.json", 'w') as json_file:
             json.dump(result_dict, json_file)
 
