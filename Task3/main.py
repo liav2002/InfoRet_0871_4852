4bacc12059c561fc8a43c2e7a7ed1b50f7c4630d
nyosef1108 <nyosef@g.jct.ac.il>
2024-02-19 21:16:31 +0200

    tagged_translated_HeBERT_15000_docs

--- Task3/main.py
+++ Task3/main.py
@@ -2,7 +2,8 @@ import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from tqdm import tqdm
 import json
-
+from HeBERT.src.HebEMO import *
+from transformers import AutoTokenizer, AutoModel, pipeline
 
 def analyze_sentiment_with_progress(text, pbar, result_dict):
     sid = SentimentIntensityAnalyzer()
@@ -43,21 +44,42 @@ def process_excel_file(file_path, result_dict):
 
 
 def main():
-    file_paths = [
-        "./data/translated_15000_docs/translated_A.xlsx",
-        "./data/translated_15000_docs/translated_B.xlsx",
-        "./data/translated_15000_docs/translated_C.xlsx"
-    ]
+    # file_paths = [
+    #     "./data/translated_15000_docs/translated_A.xlsx",
+    #     "./data/translated_15000_docs/translated_B.xlsx",
+    #     "./data/translated_15000_docs/translated_C.xlsx"
+    # ]
+    #
+    # for file_path in file_paths:
+    #     group_id = file_path.split('_')[-1][0]
+    #     result_dict = {'Positive': 0, 'Negative': 0, 'Neutral': 0}
+    #     output_file_path = process_excel_file(file_path, result_dict)
+    #     with open(f"./output/tagged_translated_15000_docs/result_{group_id}.json", 'w') as json_file:
+    #         json.dump(result_dict, json_file)
+    #
+    # print("Sentiment analysis completed for all files.")
 
-    for file_path in file_paths:
-        group_id = file_path.split('_')[-1][0]
-        result_dict = {'Positive': 0, 'Negative': 0, 'Neutral': 0}
-        output_file_path = process_excel_file(file_path, result_dict)
-        with open(f"./output/tagged_translated_15000_docs/result_{group_id}.json", 'w') as json_file:
-            json.dump(result_dict, json_file)
+    # HebEMO_model = HebEMO()
+    # print("HebEMO_model = HebEMO()")
+    # HebEMO_model.hebemo(input_path='examples/text_example.txt')
+    # print("HebEMO_model.hebemo(input_path='examples/text_example.txt')")
+    # hebEMO_df = HebEMO_model.hebemo(text='החיים יפים ומאושרים', plot=True)
+    # print("hebEMO_df = HebEMO_model.hebemo(text='החיים יפים ומאושרים', plot=True)")
 
-    print("Sentiment analysis completed for all files.")
-
+    print(0)
+    tokenizer = AutoTokenizer.from_pretrained("avichr/heBERT_sentiment_analysis")  # same as 'avichr/heBERT' tokenizer
+    print(1)
+    model = AutoModel.from_pretrained("avichr/heBERT_sentiment_analysis")
+    print(2)
+    sentiment_analysis = pipeline(
+        "sentiment-analysis",
+        model="avichr/heBERT_sentiment_analysis",
+        tokenizer="avichr/heBERT_sentiment_analysis",
+        return_all_scores=True
+    )
+    print(3)
+    sentiment_analysis('אני מתלבט מה לאכול לארוחת צהריים')
+    print(4)
 
 if __name__ == "__main__":
     main()
